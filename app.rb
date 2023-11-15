900acb307545f90ff83f72d27fc04c4def645448
maximoortelli <maximoortelli@hotmail.com>
2023-11-15 13:14:09 -0300

    Update app.rb and app_interface.rb
    Update app.rb and app_interface.rb

--- app.rb
+++ app.rb
@@ -1,177 +1,92 @@
+require_relative 'book'
 require_relative 'person'
-require_relative 'classroom'
 require_relative 'student'
 require_relative 'teacher'
-require_relative 'book'
 require_relative 'rental'
+require_relative 'classroom'
+require_relative 'app_interface'
 
 class App
+  attr_accessor :books, :people, :rentals
+
   def initialize
-    @people = []
     @books = []
+    @people = []
     @rentals = []
+    @app_interface = AppInterface.new(self)
   end
 
-  def display_menu
-    puts 'Welcome to the School Library App'
-    puts 'Please choose an option by entering a number:'
-    puts '1. List all books'
-    puts '2. List all people'
-    puts '3. Create a person'
-    puts '4. Create a book'
-    puts '5. Create a rental'
-    puts '6. List all rentals for a given person id'
-    puts '7. Exit'
+  def run
+    @app_interface.run
   end
 
-  def list_books
-    puts 'List of Books:'
-    @books.each { |book| puts "#{book.title} by #{book.author}" }
-  end
-
-  def list_people
-    puts 'List of People:'
-    @people.each do |person|
-      if person.is_a?(Teacher)
-        puts "[Teacher] Name: #{person.name} ID: #{person.id}: Age: #{person.age}"
-      elsif person.is_a?(Student)
-        puts "[Student] Name: #{person.name} ID: #{person.id}: Age: #{person.age}"
-      else
-        puts "Person Name: #{person.name} ID: #{person.id}: Age: #{person.age}"
-      end
-    end
-  end
-
-  def create_person
-    puts 'Create a Person'
-    puts 'Do you want to create a student (1) or a teacher (2)? [Input the number]:'
-    person_type = gets.chomp.to_i
-
+  def create_person(person_type)
     case person_type
-    when 1
-      create_student
-    when 2
-      create_teacher
+    when '1'
+      student = create_student
+      @people << student
+      puts 'Student created successfully'
+    when '2'
+      teacher = create_teacher
+      @people << teacher
+      puts 'Teacher created successfully'
     else
-      puts 'Invalid person type'
+      puts 'Invalid option'
     end
   end
 
   def create_student
-    puts 'Create a Student'
-    puts 'Age:'
-    age = gets.chomp.to_i
-    puts 'Name:'
-    name = gets.chomp
-    puts 'Has parent permission? [Y/N]:'
-    parent_permission = gets.chomp.downcase == 'y'
+    name = @app_interface.request_input('Name: ')
+    age = @app_interface.request_input('Age: ').to_i
+    classroom_name = @app_interface.request_input('Classroom: ')
+    classroom = Classroom.new(classroom_name)
+    parent_permission = @app_interface.request_yes_no('Has parent permission? [Y/N]: ')
 
-    student = Student.new(name, age, parent_permission: parent_permission)
-    @people << student
-    puts 'Student created successfully'
+    Student.new(name, age, classroom, parent_permission: parent_permission)
   end
 
   def create_teacher
-    puts 'Create a Teacher'
-    puts 'Age:'
-    age = gets.chomp.to_i
-    puts 'Name:'
-    name = gets.chomp
-    puts 'Specialization:'
-    specialization = gets.chomp
+    name = @app_interface.request_input('Name: ')
+    age = @app_interface.request_input('Age: ').to_i
+    specialization = @app_interface.request_input('Specialization: ')
 
-    teacher = Teacher.new(name, age, parent_permission: true, specialization: specialization)
-    @people << teacher
-    puts 'Teacher created successfully'
+    Teacher.new(name, age, specialization)
   end
 
   def create_book
-    puts 'Create a Book'
-    puts 'Title:'
-    title = gets.chomp
-    puts 'Author:'
-    author = gets.chomp
+    title = @app_interface.request_input('Title: ')
+    author = @app_interface.request_input('Author: ')
 
-    book = Book.new(title, author)
-    @books << book
+    @books << Book.new(title, author)
     puts 'Book created successfully'
   end
 
-  def book_number
-    puts 'Select a book from the following list by number:'
-    display_books
-    gets.chomp.to_i
-  end
-
-  def person_number
-    puts 'Select a person from the following list by number:'
-    display_people
-    gets.chomp.to_i
-  end
-
   def create_rental
-    book_number = book_number_from_user
-    person_number = person_number_from_user
-    puts 'Enter rental date (YYYY-MM-DD):'
-    date = gets.chomp
+    book_index = @app_interface.select_from_list('Select a book', @books)
+    person_index = @app_interface.select_from_list('Select a person', @people)
+    date = @app_interface.request_input('Date: ')
 
-    rental = Rental.new(date, @books[book_number], @people[person_number])
-    @rentals << rental
+    @rentals << Rental.new(date, @books[book_index], @people[person_index])
     puts 'Rental created successfully'
   end
 
-  def book_number_from_user
-    puts 'Select a book from the following list by number:'
-    display_books
-    gets.chomp.to_i
-  end
+  def list_rentals_for_person
+    person_id = @app_interface.request_input('Enter person id: ').to_i
 
-  def person_number_from_user
-    puts 'Select a person from the following list by number:'
-    display_people
-    gets.chomp.to_i
-  end
+    rentals = @rentals.select { |rental| rental.person.id == person_id }
 
-  def display_books
-    @books.each_with_index do |book, index|
-      puts "#{index}) Title: \"#{book.title}\", Author: #{book.author}"
-    end
-  end
-
-  def display_people
-    @people.each_with_index do |person, index|
-      if person.is_a?(Teacher)
-        puts "#{index}) [Teacher] Name: #{person.name}, ID: #{person.id}, Age: #{person.age}"
-      elsif person.is_a?(Student)
-        puts "#{index}) [Student] Name: #{person.name}, ID: #{person.id}, Age: #{person.age}"
-      else
-        puts "#{index}) Person Name: #{person.name}, ID: #{person.id}, Age: #{person.age}"
+    if rentals.empty?
+      puts 'No rentals found for the given person id.'
+    else
+      puts 'Rentals:'
+      rentals.each do |rental|
+        puts "Date: #{rental.date}, Book: #{rental.book.title} by #{rental.book.author}"
       end
     end
   end
 
-  def list_rentals_for_person
-    puts 'Select a person from the following list by number:'
-    display_people
-    person_number = gets.chomp.to_i
-
-    person = @people[person_number]
-    unless person
-      puts 'Invalid person number'
-      return
-    end
-
-    rentals = @rentals.select { |rental| rental.person == person }
-    if rentals.empty?
-      puts "No rentals found for #{person.name}"
-    else
-      puts "Rentals for #{person.name}:"
-      rentals.each { |rental| puts "#{rental.book.title}, Date: #{rental.date}" }
-    end
-  end
-
   def exit_app
-    puts 'Exiting the app...'
+    puts 'Exiting the application. Goodbye!'
     exit
   end
 end
--- app_interface.rb
+++ app_interface.rb
@@ -1,10 +1,12 @@
+# app_interface.rb
+
 require_relative 'app'
 
 class AppInterface
   ACTIONS = {
     1 => :list_books,
     2 => :list_people,
-    3 => :create_person,
+    3 => :create_person_option,
     4 => :create_book,
     5 => :create_rental,
     6 => :list_rentals_for_person,
@@ -13,12 +15,6 @@ class AppInterface
 
   def initialize(app)
     @app = app
-  end
-
-  def create_person_option
-    puts 'Do you want to create a student (1) or a teacher (2)? [Input the number]: '
-    person_type = gets.chomp
-    @app.create_person(person_type)
   end
 
   def run
@@ -38,14 +34,15 @@ class AppInterface
   def handle_option(option)
     action = ACTIONS[option]
     if action
-      case action
-      when :create_person
-        create_person_option
-      else
-        @app.send(action)
-      end
+      send(action)
     else
       puts 'Invalid option. Please try again.'
     end
+  end
+
+  def create_person_option
+    puts 'Do you want to create a student (1) or a teacher (2)? [Input the number]: '
+    person_type = gets.chomp
+    @app.create_person(person_type)
   end
 end
